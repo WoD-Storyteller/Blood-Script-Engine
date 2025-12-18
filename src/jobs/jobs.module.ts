715ace9a3ed2fc98baa0477d5b10356f983fdadc
WoD-Storyteller <gurutim111@gmail.com>
2025-12-18 13:46:41 +0000

    update src/jobs/jobs.module.ts

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
+import { EngineBootstrapService } from './engine-bootstrap.service';
 import { DatabaseModule } from '../database/database.module';
 import { PoliticsModule } from '../politics/politics.module';
 
@@ -8,6 +9,9 @@ import { PoliticsModule } from '../politics/politics.module';
     DatabaseModule,
     PoliticsModule,
   ],
-  providers: [JobsService],
+  providers: [
+    JobsService,
+    EngineBootstrapService,
+  ],
 })
 export class JobsModule {}
