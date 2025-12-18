ede730212ef24a5240691744092d6d4cf8784fa9
WoD-Storyteller <gurutim111@gmail.com>
2025-12-18 13:38:40 +0000

    update src/companion/companion.module.ts

--- src/companion/companion.module.ts
+++ src/companion/companion.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CompanionAuthService } from './auth.service';
 import { DashboardService } from './dashboard.service';
+import { CompanionController } from './companion.controller';
 
 @Module({
   providers: [CompanionAuthService, DashboardService],
+  controllers: [CompanionController],
   exports: [CompanionAuthService, DashboardService],
 })
 export class CompanionModule {}
