cc68e4aa6f3e4b755719b4e5e8197839214ce2dc
WoD-Storyteller <gurutim111@gmail.com>
2025-12-18 12:49:17 +0000

    update src/politics/politics.module.ts

--- src/politics/politics.module.ts
+++ src/politics/politics.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
+
 import { BoonsService } from './boons.service';
 import { FactionsService } from './factions.service';
 import { DomainsService } from './domains.service';
 import { OfficesService } from './offices.service';
 import { MotionsService } from './motions.service';
 import { PrestigeService } from './prestige.service';
-
-import { CoteriesAdapter } from './coteries.adapter';
-import { HoldingsService } from './holdings.service';
-import { TaxService } from './tax.service';
-import { BoonEnforcementService } from './boon-enforcement.service';
 import { NightCycleService } from './night-cycle.service';
 
 import { ChronicleModule } from '../chronicle/chronicle.module';
+import { ThreatsModule } from '../threats/threats.module';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
-  imports: [ChronicleModule],
+  imports: [
+    ChronicleModule,
+    ThreatsModule,
+    AiModule,
+  ],
   providers: [
     BoonsService,
     FactionsService,
@@ -23,10 +25,6 @@ import { ChronicleModule } from '../chronicle/chronicle.module';
     OfficesService,
     MotionsService,
     PrestigeService,
-    CoteriesAdapter,
-    HoldingsService,
-    TaxService,
-    BoonEnforcementService,
     NightCycleService,
   ],
   exports: [
@@ -36,9 +34,6 @@ import { ChronicleModule } from '../chronicle/chronicle.module';
     OfficesService,
     MotionsService,
     PrestigeService,
-    HoldingsService,
-    TaxService,
-    BoonEnforcementService,
     NightCycleService,
   ],
 })
