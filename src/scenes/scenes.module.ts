8642194921de8e657611f6d0a516108e2f78d5d5
WoD-Storyteller <gurutim111@gmail.com>
2025-12-18 01:22:49 +0000

    update src/scenes/scenes.module.ts

--- src/scenes/scenes.module.ts
+++ src/scenes/scenes.module.ts
@@ -3,21 +3,23 @@ import { ScenesService } from './scenes.service';
 import { StCoreService } from './st-core.service';
 import { ResolutionPipeline } from './resolution.pipeline';
 import { SafetyModule } from '../safety/safety.module';
-import { CombatModule } from '../combat/combat.module';
-import { PoliticsModule } from '../politics/politics.module';
-
 import { DiceService } from '../rules/dice.service';
 import { HungerService } from '../rules/hunger.service';
 import { WillpowerService } from '../rules/willpower.service';
 import { PresenceService } from './presence.service';
+import { CombatModule } from '../combat/combat.module';
+
 import { DisciplineService } from '../rules/discipline.service';
 import { RouseService } from '../rules/rouse.service';
 import { CharacterContextService } from './character-context.service';
 import { StatusService } from './status.service';
 import { RecoveryService } from './recovery.service';
 
+import { PoliticsModule } from '../politics/politics.module';
+import { ChronicleModule } from '../chronicle/chronicle.module';
+
 @Module({
-  imports: [SafetyModule, CombatModule, PoliticsModule],
+  imports: [SafetyModule, CombatModule, PoliticsModule, ChronicleModule],
   providers: [
     ScenesService,
     StCoreService,
