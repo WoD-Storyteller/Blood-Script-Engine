c6e2907ba0d39e2374e82209dd8a682fd079422d
WoD-Storyteller <gurutim111@gmail.com>
2025-12-19 20:17:24 +0000

    update src/realtime/realtime.module.ts

--- src/realtime/realtime.module.ts
+++ src/realtime/realtime.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { CompanionModule } from '../companion/companion.module';
+import { DashboardModule } from '../companion/dashboard.module';
+
 import { RealtimeGateway } from './realtime.gateway';
 import { RealtimeService } from './realtime.service';
 
 @Module({
-  imports: [DatabaseModule, CompanionModule],
+  imports: [DatabaseModule, CompanionModule, DashboardModule],
   providers: [RealtimeGateway, RealtimeService],
   exports: [RealtimeService],
 })
