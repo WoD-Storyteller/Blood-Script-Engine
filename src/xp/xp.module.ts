8d570c20172f7d66d0ec3326bdd5ebfbb53217c4
WoD-Storyteller <gurutim111@gmail.com>
2025-12-19 19:47:06 +0000

    update src/xp/xp.module.ts

---
+++ src/xp/xp.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { CompanionModule } from '../companion/companion.module';
+import { CharactersModule } from '../characters/characters.module';
+
+import { XpService } from './xp.service';
+import { XpController } from './xp.controller';
+
+@Module({
+  imports: [DatabaseModule, CompanionModule, CharactersModule],
+  providers: [XpService],
+  controllers: [XpController],
+  exports: [XpService],
+})
+export class XpModule {}
