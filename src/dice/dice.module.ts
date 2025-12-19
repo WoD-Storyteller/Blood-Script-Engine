c6dbd5a811a93779c28508fc8e74761ec4a8fd62
WoD-Storyteller <gurutim111@gmail.com>
2025-12-19 19:38:16 +0000

    update src/dice/dice.module.ts

--- src/dice/dice.module.ts
+++ src/dice/dice.module.ts
@@ -1,1 +1,20 @@
 
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { CompanionModule } from '../companion/companion.module';
+import { CharactersModule } from '../characters/characters.module';
+
+import { DiceService } from './dice.service';
+import { DiceController } from './dice.controller';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    CompanionModule,
+    CharactersModule,
+  ],
+  providers: [DiceService],
+  controllers: [DiceController],
+  exports: [DiceService],
+})
+export class DiceModule {}
