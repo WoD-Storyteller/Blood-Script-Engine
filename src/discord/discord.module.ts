fe47264fa15b8f7cb2d15f9883d52bde50327c2a
WoD-Storyteller <gurutim111@gmail.com>
2025-12-19 21:20:03 +0000

    update src/discord/discord.module.ts

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { DiscordService } from './discord.service';
-import { ScenesModule } from '../scenes/scenes.module';
+import { DiscordDmService } from './discord.dm.service';
+import { ConfigModule } from '@nestjs/config';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 @Module({
-  imports: [ScenesModule],
-  providers: [DiscordService],
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: Client,
+      useFactory: () => {
+        const client = new Client({
+          intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.DirectMessages,
+          ],
+        });
+        client.login(process.env.DISCORD_BOT_TOKEN);
+        return client;
+      },
+    },
+    DiscordService,
+    DiscordDmService,
+  ],
+  exports: [DiscordService, DiscordDmService, Client],
 })
 export class DiscordModule {}
