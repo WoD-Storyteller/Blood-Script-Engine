34b1223c38e9acf4d61c04467ab7a871d30f7cd9
WoD-Storyteller <gurutim111@gmail.com>
2025-12-19 01:56:38 +0000

    update src/companion/companion.module.ts

--- src/companion/companion.module.ts
+++ src/companion/companion.module.ts
@@ -4,15 +4,19 @@ import { DashboardService } from './dashboard.service';
 import { CompanionController } from './companion.controller';
 import { CharactersService } from './characters.service';
 import { CoteriesService } from './coteries.service';
+import { StAdminService } from './st-admin.service';
+
 import { WorldModule } from '../world/world.module';
+import { ChronicleModule } from '../chronicle/chronicle.module';
 
 @Module({
-  imports: [WorldModule],
+  imports: [WorldModule, ChronicleModule],
   providers: [
     CompanionAuthService,
     DashboardService,
     CharactersService,
     CoteriesService,
+    StAdminService,
   ],
   controllers: [CompanionController],
   exports: [
@@ -20,6 +24,7 @@ import { WorldModule } from '../world/world.module';
     DashboardService,
     CharactersService,
     CoteriesService,
+    StAdminService,
   ],
 })
 export class CompanionModule {}
