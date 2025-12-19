9bf21c94913832228351ed366dc543f2f70f3516
WoD-Storyteller <gurutim111@gmail.com>
2025-12-19 02:35:29 +0000

    update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableShutdownHooks();
-  await app.listen(process.env.PORT || 3000);
+
+  const appUrl = process.env.COMPANION_APP_URL || 'http://localhost:5173';
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: appUrl,
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
 bootstrap();
