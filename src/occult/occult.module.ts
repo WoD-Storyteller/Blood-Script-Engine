fde58b156a3adc0ee9e0cda980f9be25afd1ec93
WoD-Storyteller <gurutim111@gmail.com>
2025-12-20 17:16:16 +0000

    Create OccultModule with controller and service

--- src/occult/occult.module.ts
+++ src/occult/occult.module.ts
@@ -1,1 +1,13 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 
+import { OccultController } from './occult.controller';
+import { OccultService } from './occult.service';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [OccultController],
+  providers: [OccultService],
+  exports: [OccultService],
+})
+export class OccultModule {}
