5017dcffe5ae5900416d43d35f881a404ee82abd
WoD-Storyteller <gurutim111@gmail.com>
2025-12-27 15:43:01 +0000

    Update st-core.service.ts

--- src/scenes/st-core.service.ts
+++ src/scenes/st-core.service.ts
@@ -1,43 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { ClocksService } from '../chronicle/clocks.service';
 import { ArcsService } from '../chronicle/arcs.service';
-import { IntentsService } from '../scenes/intents.service';
 
 @Injectable()
-export class StAdminService {
+export class StCoreService {
   constructor(
     private readonly clocks: ClocksService,
     private readonly arcs: ArcsService,
-    private readonly intents: IntentsService,
   ) {}
 
-  async createClock(client: any, engineId: string, input: any) {
-    return this.clocks.create(client, engineId, input);
+  createClock(client: any, engineId: string, input: any) {
+    return this.clocks.createClock(client, {
+      engineId,
+      ...input,
+    });
   }
 
-  async tickClock(
-    client: any,
-    engineId: string,
-    clockId: string,
-    delta: number,
-  ) {
-    return this.clocks.tick(client, engineId, clockId, delta);
+  tickClock(client: any, engineId: string, clockId: string, delta = 1) {
+    return this.clocks.tickClock(client, {
+      engineId,
+      clockId,
+      delta,
+    });
   }
 
-  async createArc(client: any, engineId: string, input: any) {
-    return this.arcs.create(client, engineId, input);
+  createArc(client: any, engineId: string, input: any) {
+    return this.arcs.createArc(client, {
+      engineId,
+      ...input,
+    });
   }
 
-  async setArcStatus(
-    client: any,
-    engineId: string,
-    arcId: string,
-    status: string,
-  ) {
-    return this.arcs.setStatus(client, engineId, arcId, status);
-  }
-
-  async listIntents(client: any, engineId: string) {
-    return this.intents.list(client, engineId);
+  setArcStatus(client: any, engineId: string, arcIdPrefix: string, status: any) {
+    return this.arcs.setStatus(client, {
+      engineId,
+      arcIdPrefix,
+      status,
+    });
   }
 }
