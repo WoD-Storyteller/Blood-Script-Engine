378ed707302f477a6dafe2841d2c32d85c9e1757
WoD-Storyteller <gurutim111@gmail.com>
2025-12-27 16:29:08 +0000

    Update st-admin.service.ts

--- src/companion/st-admin.service.ts
+++ src/companion/st-admin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-import { ClocksService } from '../scenes/clocks.service';
+import { ClocksService } from '../chronicle/clocks.service';
 import { MapsService } from '../engine/maps.service';
 import { ArcsService } from '../chronicle/arcs.service';
 import { ArcStatus } from '../chronicle/arcs.enums';
@@ -16,10 +16,7 @@ export class StAdminService {
   ) {}
 
   setMap(client: any, engineId: string, url: string) {
-    return this.maps.setMap(client, {
-      engineId,
-      url,
-    });
+    return this.maps.setMap(client, { engineId, url });
   }
 
   createClock(client: any, engineId: string, body: any) {
@@ -32,13 +29,8 @@ export class StAdminService {
     });
   }
 
-  tickClock(
-    client: any,
-    engineId: string,
-    clockIdPrefix: string,
-    delta = 1,
-  ) {
-    return this.clocks.tick(client, {
+  tickClock(client: any, engineId: string, clockIdPrefix: string, delta = 1) {
+    return this.clocks.tickClock(client, {
       engineId,
       clockIdPrefix,
       amount: delta,
@@ -77,12 +69,7 @@ export class StAdminService {
     return this.intents.approve(client, engineId, intentId);
   }
 
-  rejectIntent(
-    client: any,
-    engineId: string,
-    intentId: string,
-    reason?: string,
-  ) {
+  rejectIntent(client: any, engineId: string, intentId: string, reason?: string) {
     return this.intents.reject(client, engineId, intentId, reason);
   }
 }
