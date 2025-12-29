812b3183da543de8d3c6df07480d7f8be350c7e4
WoD-Storyteller <gurutim111@gmail.com>
2025-12-29 12:15:39 +0000

    Update dashboard.module.ts

--- src/companion/dashboard.module.ts
+++ src/companion/dashboard.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { WorldModule } from '../world/world.module';
 import { DashboardService } from './dashboard.service';
 
 @Module({
   imports: [
     DatabaseModule,
+    WorldModule, // 🔑 Gives access to MapsService
   ],
   providers: [
     DashboardService,
   ],
   exports: [
-    DashboardService, // 🔑 REQUIRED for RealtimeModule
+    DashboardService,
   ],
 })
 export class DashboardModule {}
