2535aa3b069577859c0d2388a5afca4dacd58fb9
WoD-Storyteller <gurutim111@gmail.com>
2025-12-29 21:51:46 +0000

    Update xp.module.ts

--- src/xp/xp.module.ts
+++ src/xp/xp.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { CompanionModule } from '../companion/companion.module';
 import { CharactersModule } from '../characters/characters.module';
-
+import { RealtimeModule } from '../realtime/realtime.module';
 import { XpService } from './xp.service';
 import { XpController } from './xp.controller';
 
 @Module({
-  imports: [DatabaseModule, CompanionModule, CharactersModule],
+  imports: [
+    DatabaseModule,
+    CompanionModule,
+    CharactersModule,
+    RealtimeModule,
+  ],
   providers: [XpService],
   controllers: [XpController],
   exports: [XpService],
