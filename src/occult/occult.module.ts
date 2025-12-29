675b3ad4d68f03e7e78f5a28ede6fa3c36bb8828
WoD-Storyteller <gurutim111@gmail.com>
2025-12-29 21:38:24 +0000

    Update occult.module.ts

--- src/occult/occult.module.ts
+++ src/occult/occult.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
-
+import { CompanionModule } from '../companion/companion.module';
 import { OccultController } from './occult.controller';
 import { OccultService } from './occult.service';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [OccultController],
-  providers: [OccultService],
-  exports: [OccultService],
+  imports: [
+    DatabaseModule,
+    CompanionModule
+  ],
+  controllers: [
+    OccultController
+  ],
+  providers: [
+    OccultService
+  ],
+  exports: [
+    OccultService
+  ],
 })
 export class OccultModule {}
