0663b29725d1409877f9690b727e57cc9a32aee3
WoD-Storyteller <gurutim111@gmail.com>
2025-12-29 01:31:45 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,18 +3,11 @@ module.exports = {
     {
       name: 'blood-script-engine',
       script: 'dist/main.js',
-      instances: 1,
-      exec_mode: 'fork',
-
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
+        GCP_PROJECT_ID: 'just-sunrise-398717',
       },
-
-      max_memory_restart: '512M',
-      autorestart: true,
-      watch: false,
-      time: true,
     },
   ],
 };
