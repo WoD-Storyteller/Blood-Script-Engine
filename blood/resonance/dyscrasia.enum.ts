decbc70622ee84c39b4c6674bfc2afb866b79ab1
WoD-Storyteller <gurutim111@gmail.com>
2026-01-13 17:55:21 +0000

    Delete dyscrasia.enum.ts

--- blood/resonance/dyscrasia.enum.ts
+++
@@ -1,8 +0,0 @@
-// src/blood/resonance/dyscrasia.enum.ts
-
-export enum DyscrasiaType {
-  CHOLERIC = 'choleric',
-  SANGUINE = 'sanguine',
-  MELANCHOLIC = 'melancholic',
-  PHLEGMATIC = 'phlegmatic',
-}
