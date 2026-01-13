e847e2ac5a859611aa9d72b9019da4790bdca643
WoD-Storyteller <gurutim111@gmail.com>
2026-01-13 17:41:16 +0000

    Update resonance.module.ts

--- blood/resonance/resonance.module.ts
+++ blood/resonance/resonance.module.ts
@@ -4,17 +4,20 @@ import { Module } from '@nestjs/common'
 import { ResonanceService } from './resonance.service'
 import { DyscrasiaService } from './dyscrasia.service'
 import { BloodHooksService } from './blood-hooks.service'
+import { DisciplineResonanceService } from './discipline-resonance.service'
 
 @Module({
   providers: [
     ResonanceService,
     DyscrasiaService,
     BloodHooksService,
+    DisciplineResonanceService,
   ],
   exports: [
     ResonanceService,
     DyscrasiaService,
     BloodHooksService,
+    DisciplineResonanceService,
   ],
 })
 export class ResonanceModule {}
