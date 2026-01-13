c647ff7fae9a6f707ce78ec3c1cc7f2ec645450c
WoD-Storyteller <gurutim111@gmail.com>
2026-01-13 17:35:18 +0000

    Create resonance.enum.ts

---
+++ blood/resonance/resonance.enum.ts
@@ -0,0 +1,13 @@
+// src/blood/resonance/resonance.enum.ts
+
+export enum ResonanceType {
+  CHOLERIC = 'choleric',
+  SANGUINE = 'sanguine',
+  MELANCHOLIC = 'melancholic',
+  PHLEGMATIC = 'phlegmatic',
+}
+
+export enum ResonanceIntensity {
+  FAINT = 'faint',
+  INTENSE = 'intense',
+}
