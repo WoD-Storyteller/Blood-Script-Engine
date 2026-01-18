cb1f5c34595360a01984818bb6941bf55593d18b
WoD-Storyteller <gurutim111@gmail.com>
2026-01-18 01:56:23 +0000

    Add blood potency torpor, diablerie, and clocks

--- src/blood-potency/blood-potency.module.ts
+++ src/blood-potency/blood-potency.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BloodPotencyProgressionService } from './blood-potency.progression.service';
 import { BloodPotencyService } from './blood-potency.service';
 
 @Module({
-  providers: [BloodPotencyService],
-  exports: [BloodPotencyService],
+  providers: [BloodPotencyService, BloodPotencyProgressionService],
+  exports: [BloodPotencyService, BloodPotencyProgressionService],
 })
 export class BloodPotencyModule {}
